13d45af0350d4d57035d284542426ddea19cfe45
Arpit1718 <101792247+Arpit1718@users.noreply.github.com>
2023-09-19 22:04:38 +0530

    Add files via upload

--- Error.sol
+++ Error.sol
@@ -1,12 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity = 0.8.18; //solidity version
+pragma solidity ^0.8.18;
 
-contract  Exception {
-
+contract Exception {
     uint256 public value;
 
     // Function to set a new value, but only if the new value is greater than the current value
-
     function setValue(uint256 newValue) public {
         // Use require() to validate the condition
         require(newValue > value, "New value must be greater than the current value.");
@@ -16,16 +14,17 @@ contract  Exception {
 
     // Function to get the square of a number, but only if the number is positive
     function getSquare(uint256 number) public pure returns (uint256) {
-       
         // Use assert() to check an invariant condition (a condition that should never be false)
         assert(number >= 0);
 
         return number * number;
     }
 
-    // Function that always reverts with a custom error message
-    function alwaysRevert() public pure {
-        // Use revert() to explicitly revert the transaction  with a custom error message
-        revert("This function always reverts.");
+    // Function that may trigger the revert error handler
+    function triggerRevert(bool shouldRevert) public pure {
+        if (shouldRevert) {
+            // Use revert() to explicitly revert the transaction with a custom error message
+            revert("This transaction was intentionally reverted.");
+        }
     }
 }
